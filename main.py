84ff4e1ff7cf0976d17fec9702c79833d9296014
Yevheniia22 <yevheniia.shpak@gmail.com>
2023-10-22 20:52:13 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,19 @@
+from flask import Flask, render_template
+app = Flask(__name__)
+menus = [
+    {"pizza": "margarita", "price": 100},
+    {"pizza": "paperoni", "price": 78},
+    {"pizza": "philadelfia", "price": 65},
+    {"pizza": "bawarska", "price": 84},
+    {"pizza": "duavola", "price": 99}
+]
+
+@app.route('/')
+@app.route('/index/')
+def index():
+    context = {
+        'pizzas': menus
+    }
+    return render_template('index.html', **context)
+
+app.run(debug=True)
