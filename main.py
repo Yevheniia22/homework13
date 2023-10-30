beb572db7536dbee5eb3fee26331d0b229de5277
Yevheniia22 <yevheniia.shpak@gmail.com>
2023-10-30 19:58:52 +0100

    commit_22_10

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 import sqlite3
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, url_for
+
+ADMIN_USERNAME = "admin"
+
 app = Flask(__name__)
+
 menu = [
     {"pizza": "margarita", "description": "bla bla", "price": 100},
     {"pizza": "paperoni", "description": "bla bla", "price": 78},
@@ -22,22 +26,30 @@ connect = sqlite3.connect("app.db")
 cursor = connect.cursor()
 cursor.execute("CREATE TABLE IF NOT EXISTS menu(pizza TEXT, description TEXT, price TEXT)")
 
-@app.route("/menu_list/", methods=['GET', 'POST'])
+def is_admin(username, password):
+    return username == ADMIN_USERNAME
+
+@app.route("/join/", methods=['GET', 'POST'])
 def join():
     if request.method == 'POST':
-        pizza = request.form['pizza']
-        description = request.form['description']
-        price = request.form['price']
-        with sqlite3.connect("app.db") as user:
-            cursor = user.cursor()
-            cursor.execute("""INSERT INTO menu (pizza, description, price) 
-            VALUES (?, ?, ?)""", (pizza, description, price))
-            user.commit()
-            return render_template("index.html")
-    else:
-        return render_template("index.html")
+        username = request.form['username']
 
-@app.route('/menu/')
+        if is_admin(username):
+            pizza = request.form['pizza']
+            description = request.form['description']
+            price = request.form['price']
+
+            with sqlite3.connect("app.db") as user:
+                cursor = user.cursor()
+                cursor.execute("INSERT INTO menu (pizza, description, price) VALUES (?, ?, ?)", (pizza, description, price))
+                user.commit()
+                return render_template("join.html")
+        else:
+            return "Unauthorized", 401
+
+    return render_template("join.html")
+
+@app.route('/menu_list/')
 def menu():
     connect = sqlite3.connect("app.db")
     cursor = connect.cursor()
